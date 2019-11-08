31bc0bcaa64bdfd24def927ca8f09c822839eef7
Ben Carlson <bjcarlson42@outlook.com>
2019-11-07 20:16:08 -0500

    5. Python Tutorial 2: Variables, Comments, and Data Types

---
+++ 5. Beginner Python Tutorial 2/program.py
@@ -0,0 +1,40 @@
+#@author: Benjamin Carlson
+# computersciencecentral.dev
+# benjamincarlson.net
+#date: 11/6/19
+#This is the code used in the tutorial https://www.computersciencecentral.dev/python-tutorial-2/
+#you can uncomment the lines one at a time to follow along.
+
+x = 10 #Assigned the value 10 to the variable x
+
+print(x)
+print(x + 5) #adds the variable x and 5 and prints the result
+
+print(9999999999999999999999)
+print(type(10))
+
+floating_point_number = 3.14
+print(type(floating_point_number))
+print(type(2.34))
+
+my_string = "hi"
+my_string2 = 'hello again'
+print(type(my_string))
+print(type('i am a string'))
+
+print(my_string2[0])
+print(my_string2[1])
+
+#escape sequences
+#print('I don't want to use double quotations.') - doesn't work
+print('I don\'t want to use double quotations.')
+
+print("""Hello. This is a double " quote and this is a single ' quote.""")
+#print(""Hello. This is a double " quote and this is a single ' quote."") - doesn't work
+
+#print("string') - doesn't work
+#print("""string"") - doesn't work
+
+my_boolean = True
+print(type(my_boolean))
+print(type(False))
